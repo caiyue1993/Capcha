3a80c7901ec6a10b3a918e776840c24cd57fda51
caiyue1993 <yue.cai@shanbay.com>
2016-12-06 18:40:55 +0800

    In memory of the very first init of Capcha

---
+++ Capcha/Capcha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Capcha
+//
+//  Created by 蔡越 on 16/12/6.
+//  Copyright © 2016年 Nanjing University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
