954a013d86adbaaf42224ddcdb906558199614a5
caiyue1993 <yue.cai@shanbay.com>
2016-12-19 00:33:34 +0800

    configure ModelViewController and PhotographerViewController

--- Capcha/Capcha/ModelViewController.swift
+++ Capcha/Capcha/ModelViewController.swift
@@ -12,6 +12,10 @@ class ModelViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .orange
     }
-
+    
+    public func refresh() {
+        print("Model vc refresh")
+    }
 }
--- Capcha/Capcha/PhotographerViewController.swift
+++ Capcha/Capcha/PhotographerViewController.swift
@@ -13,16 +13,6 @@ class PhotographerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
-        setupNavBar()
-        
-    }
-    
-    func setupNavBar() {
-        let photographersButtonItem = UIBarButtonItem(title: "摄影圈", style: .plain, target: self, action: nil)
-        let modelsButtonItem = UIBarButtonItem(title: "模特圈", style: .plain, target: self
-            , action: nil)
-        modelsButtonItem.tintColor = .gray
-        self.navigationItem.leftBarButtonItems = [photographersButtonItem, modelsButtonItem]
     }
     
 }
