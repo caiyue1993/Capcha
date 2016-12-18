cf83b9a56b6c7fd3942a27bb7ba365def9500c70
caiyue1993 <yue.cai@shanbay.com>
2016-12-19 00:34:21 +0800

    test scroll code in home page

--- Capcha/Capcha/HorizontalScrollViewController.swift
+++ Capcha/Capcha/HorizontalScrollViewController.swift
@@ -7,29 +7,73 @@
 //
 
 import UIKit
+import SnapKit
 
 class HorizontalScrollViewController: UIViewController {
-
+    
+    var photographerViewController = PhotographerViewController()
+    var modelViewController = ModelViewController()
+    
+    lazy var horizontalScrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: self.view.bounds)
+        scrollView.contentSize = CGSize(width: self.view.bounds.size.width * 2, height: self.view.bounds.size.height)
+        scrollView.isPagingEnabled = true
+        scrollView.bounces = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.delegate = self
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        edgesForExtendedLayout = []
+        setupSubviews()
+        setupNavBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupSubviews() {
+        view.addSubview(self.horizontalScrollView)
+        
+        horizontalScrollView.addSubview(photographerViewController.view)
+        photographerViewController.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        
+        horizontalScrollView.addSubview(modelViewController.view)
+        modelViewController.view.frame = CGRect(x: self.view.bounds.size.width, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
     }
-    */
+    
+    func setupNavBar() {
+        let photographersButtonItem = UIBarButtonItem(title: "摄影圈", style: .plain, target: self, action: nil)
+        let modelsButtonItem = UIBarButtonItem(title: "模特圈", style: .plain, target: self
+            , action: #selector(moveToModel))
+        modelsButtonItem.tintColor = .gray
+        self.navigationItem.leftBarButtonItems = [photographersButtonItem, modelsButtonItem]
+    }
+    
+    func moveToModel() {
+        
+    }
+    
+    func barButtons() -> [UIBarButtonItem]? {
+        return self.navigationItem.leftBarButtonItems
+    }
+}
 
+extension HorizontalScrollViewController: UIScrollViewDelegate {
+    
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
+    
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / self.view.frame.size.width
+        let buttonItems = barButtons()
+        if page == 0 {
+            
+        }else {
+            buttonItems?[1].tintColor = .green
+            self.modelViewController.refresh()
+        }
+    }
 }
