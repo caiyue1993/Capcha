8be7996f358642bf896e7a90d0c03782b0362989
caiyue1993 <yue.cai@shanbay.com>
2016-12-25 22:06:43 +0800

    init files

---
+++ Capcha/Capcha/Custom/CapchaHomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CapchaHomeTableViewCell.swift
+//  Capcha
+//
+//  Created by 蔡越 on 16/12/24.
+//  Copyright © 2016年 Nanjing University. All rights reserved.
+//
+
+import UIKit
+
+class CapchaHomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
