6b7746579103174e7cf3fe286b712b28184484d5
caiyue1993 <yue.cai@shanbay.com>
2017-01-01 21:31:28 +0800

    init files

---
+++ Capcha/Capcha/Commons/CYRefreshControl.swift
@@ -0,0 +1,21 @@
+//
+//  CYRefreshControl.swift
+//  Capcha
+//
+//  Created by 蔡越 on 17/1/1.
+//  Copyright © 2017年 Nanjing University. All rights reserved.
+//
+
+import UIKit
+
+class CYRefreshControl: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Capcha/Capcha/Theme/UIColor+Capcha.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Capcha.swift
+//  Capcha
+//
+//  Created by 蔡越 on 17/1/1.
+//  Copyright © 2017年 Nanjing University. All rights reserved.
+//
+
+import Foundation
