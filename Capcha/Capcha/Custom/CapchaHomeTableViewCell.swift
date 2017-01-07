2a2fbcab67cb02a5086f54bab9fd05862028eb57
caiyue1993 <278060043@qq.com>
2017-01-07 13:23:47 +0800

    add font ttf

--- Capcha/Capcha/Custom/CapchaHomeTableViewCell.swift
+++ Capcha/Capcha/Custom/CapchaHomeTableViewCell.swift
@@ -12,17 +12,31 @@ public class CapchaHomeTableViewCell: UITableViewCell {
     
     lazy var nameLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont(name: "Kaiti SC", size: 18)
         return label
     }()
     
     lazy var locationLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont(name: "Kaiti SC", size: 20)
         return label
     }()
     
     lazy var photoImageView: UIImageView = {
         let imageView = UIImageView()
         return imageView
+    }()
+    
+    lazy var upperLine: UIView = {
+        let line = UIView()
+        line.backgroundColor = UIColor.capchaLineColor()
+        return line
+    }()
+    
+    lazy var belowLine: UIView = {
+        let line = UIView()
+        line.backgroundColor = UIColor.capchaLineColor()
+        return line
     }()
     
     class func defaultHeight() -> CGFloat {
@@ -47,16 +61,30 @@ public class CapchaHomeTableViewCell: UITableViewCell {
         contentView.addSubview(nameLabel)
         contentView.addSubview(locationLabel)
         contentView.addSubview(photoImageView)
+        contentView.addSubview(upperLine)
+        contentView.addSubview(belowLine)
     }
     
     func setupConstraints() {
         nameLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(contentView).offset(12)
-            make.leading.equalTo(contentView).offset(8)
+            make.top.equalTo(contentView).offset(18)
+            make.leading.equalTo(contentView).offset(16)
+        }
+        upperLine.snp.makeConstraints { (make) in
+            make.top.equalTo(nameLabel)
+            make.centerX.equalTo(contentView).multipliedBy(0.8)
+            make.width.equalTo(1)
+            make.height.equalTo(40)
         }
         locationLabel.snp.makeConstraints { (make) in
             make.centerY.equalTo(contentView)
             make.centerX.equalTo(contentView).multipliedBy(0.8)
+        }
+        belowLine.snp.makeConstraints { (make) in
+            make.centerX.equalTo(upperLine)
+            make.top.equalTo(locationLabel.snp.bottom).offset(20)
+            make.height.equalTo(40)
+            make.width.equalTo(upperLine)
         }
         photoImageView.snp.makeConstraints { (make) in
             make.centerY.equalTo(locationLabel)
